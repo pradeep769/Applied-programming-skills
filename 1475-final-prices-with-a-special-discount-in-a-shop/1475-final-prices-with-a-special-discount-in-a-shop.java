2263bafda89b409eca0c5ac417a1b34c0662f20b
pradeep769 <sreeram.pradeep1111@gmail.com>
2026-04-15 10:18:00 +0530

    Time: 3 ms (64.43%), Space: 45.6 MB (6.03%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] answer = prices.clone();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[i] <= prices[stack.peek()]) {
+                int idx = stack.pop();
+                answer[idx] = prices[idx] - prices[i];
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
